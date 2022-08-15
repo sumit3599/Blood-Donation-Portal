5362ff78f2fa6cc5f90218560c8a00d25cec9796
sumit3599 <81349176+sumit3599@users.noreply.github.com>
2022-08-15 14:44:00 +0530

    Add files via upload

---
+++ success.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<body>
+<center>
+<br>
+<br>
+<font color="blue" size=8>Regist</font>
+<font color="red" size=8>ration</font>
+<font color="yellow" size=8>&nbsp&nbspSucce</font>
+<font color="green" size=8>ssful</font>
+</center>
+</body>
+</html>
